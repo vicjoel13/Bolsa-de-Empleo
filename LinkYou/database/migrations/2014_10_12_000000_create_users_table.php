53ed57ad74c054a36df6e8e7064ee0991b2abdd9
vicjoel13 <62181926+vicjoel13@users.noreply.github.com>
2020-05-22 18:58:11 -0400

    Update 2014_10_12_000000_create_users_table.php

--- LinkYou/database/migrations/2014_10_12_000000_create_users_table.php
+++ LinkYou/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('type')->nullable();;
+            $table->string('type');
             $table->string('address');
             $table->string('logo');
             $table->rememberToken();
