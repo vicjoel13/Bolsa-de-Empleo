917e9382a6eb4d6709321f1646734424473dcd11
vicjoel13 <62181926+vicjoel13@users.noreply.github.com>
2020-05-23 18:52:42 -0400

    view del postJob en proceso

---
+++ LinkYou/app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        return view('user.PostJob');
+    }
+    public function store()
+    {
+        $post = new create_post_tables();
+        $post->company = request('company');
+        $post->time_type = request('time_type');
+        $post->logo = request('logo');
+        $post->URL = request('URL');
+        $post->position = request('position');
+        $post->location = request('location');
+        $post->job_category = request('job_category');
+        $post->description = request('description');
+        $post->application = request('application');
+        $post->email = request('email');
+        $post->id_company = request('id_company');
+        $post->save();
+        return view('company.indexC');
+
+    }
+}
--- LinkYou/app/create_post_table.php
+++ LinkYou/app/create_post_table.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class create_post_table extends Model
 {
-    //
+    protected $fillable = 
+    ['company', 'time_type', 'logo', 'URL', 'position',
+    'location', 'job_category', 'description', 'email', 'application', 'id_company'];
 }
