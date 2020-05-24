462438452cc99cdb1a1132e57ef2252ef07f49a8
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2020-05-23 23:27:56 -0400

    Diseno
    Diseno de Index

--- LinkYou/public/js/app.js
+++ LinkYou/public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Victor Joel\Desktop\Tareas\P3\Proyecto I\Bolsa-de-Empleos\LinkYou\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Victor Joel\Desktop\Tareas\P3\Proyecto I\Bolsa-de-Empleos\LinkYou\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Bolsa-de-Empleos\linkyou\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Bolsa-de-Empleos\linkyou\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
