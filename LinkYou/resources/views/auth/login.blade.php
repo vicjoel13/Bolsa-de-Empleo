fc0a374ca5c0263ec8d963366395d8277e75c740
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2020-05-26 14:29:17 -0400

    Update login.blade.php

--- LinkYou/resources/views/auth/login.blade.php
+++ LinkYou/resources/views/auth/login.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header" style="background: #424242;color: #FCFCFC; padding: 16px;font-size: 24px;font-weight: 300;line-height: 18px;">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
