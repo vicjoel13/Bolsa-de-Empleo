159a92947c63a324d65f08942b0dfbe0e2d5cdc5
Sahira22 <60494004+Sahira22@users.noreply.github.com>
2020-05-27 22:33:50 -0400

    Diseno De Post

--- LinkYou/resources/views/layouts/ViewP.php
+++ LinkYou/resources/views/layouts/ViewP.php
@@ -1,6 +1,169 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+        <title>LinkYou</title>
+
+        <!-- Icono -->
+        <link rel="icon" href="https://image.flaticon.com/icons/svg/121/121156.svg" type="image/x-icon">
+
+        <!-- Fonts -->
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      
+        
+      
+         <!-- Styles -->
+        <style>
+            html, body{
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                
+            }
+
+           
+        
+
+            .full-height {
+                height: 11vh;
+            }
+
+            .flex-center {
+               
+                display: flex;
+                justify-content: center;
+                border-color: transparent;
+                margin-bottom: 0;
+                z-index: 9999;
+                border: 0;
+                font-size: 12px !important;
+                line-height: 1.42857143 !important;
+                letter-spacing: 4px;
+                border-radius: 0;
+            }
+
+            .texto-encima{
+                position: absolute;
+                top: 240px;
+                right: 10px;
+}
+            .carousel-caption{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                 transform: translate(-50%, -50%);
+                 color:white;
+           }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+        
+            .carousel{
+                position:absolute;
+                top:0;
+            }
 
 
-@endsection
+            .links > a {
+                color:black;
+                padding: 0 25px;
+                font-size: 14px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+
+    <style >
+       .navbar {
+  margin-bottom: 0;
+  background-color:steelblue;
+  z-index: 9999;
+  border: 0;
+  font-size: 12px !important;
+  line-height: 1.42857143 !important;
+  letter-spacing: 4px;
+  border-radius: 0;
+}
+
+.navbar li a, .navbar .navbar-brand {
+  color: #fff ;
+}
+
+.navbar-nav li a:hover, .navbar-nav li.active a {
+  color:red !important;
+  background-color: #fff !important;
+}
+
+.navbar-default .navbar-toggle {
+  border-color: transparent;
+  color: #fff !important;
+}
+
+.container{
+
+background-color: rgb(245, 243, 253);
+}
+
+.th{
+  background-color:red;
+}
+</style>
+<nav class="navbar  navbar-default navbar-expand-lg  " >
+      
+  <a class="navbar-brand mb-0 h1" >LinkYou</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="Prestamos.php">Inicio <span class="sr-only">(current)</span></a>
+    </li>
+    
+  </ul>
+</div>
+</nav>
+
+<div class="container" >
+</div>
+      
+
+
+    
+      
+    
+        </body>
+</html>
